0132d3af765aa89fda0f44e03a4c5248b7bf885b
Gander-Alexander <Gander-Alexander@users.noreply.github.com>
2020-05-07 12:08:12 -0600

    Changed the color scheme, added a picture of me to the about page, and added more text

--- assets/hamburgerMenu.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.querySelector('.hamburgerMenu')
-
-const navLinks = document.querySelector('.nav-links')
-
-burgerMenu.addEventListener('click', function(){
-   navLinks.classList.toggle('active')
-  burgerMenu.classList.toggle('toggle') 
-})
-console.log(navLinks)
